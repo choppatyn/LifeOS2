c6c76fc6c1b221c67de960456365ca60a7b80e37
choppatyn <choppatyn@gmail.com>
2026-09-03 08:09:23 +0300

    Create index.tsx

---
+++ src/pages/Society/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Society: React.FC = () => {
+  return (
+    <div className="container">
+      <h1 className="text-xl font-bold">👥 Социум</h1>
+      <p className="text-muted text-sm mt-1">Скоро здесь будет полная информация</p>
+    </div>
+  );
+};
+
+export default Society;
