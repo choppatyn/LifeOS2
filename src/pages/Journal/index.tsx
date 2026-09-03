f122a829ef1cd7c789a2adcc3972dc15db09d6f4
choppatyn <choppatyn@gmail.com>
2026-09-03 08:10:20 +0300

    Create index.tsx

---
+++ src/pages/Journal/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Journal: React.FC = () => {
+  return (
+    <div className="container">
+      <h1 className="text-xl font-bold">📖 Дневник</h1>
+      <p className="text-muted text-sm mt-1">Скоро здесь будет полная информация</p>
+    </div>
+  );
+};
+
+export default Journal;
