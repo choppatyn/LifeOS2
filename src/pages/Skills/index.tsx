dcb8b37ff7aa079b91462c1b9b128aafa2519924
choppatyn <choppatyn@gmail.com>
2026-09-03 08:09:13 +0300

    Create index.tsx

---
+++ src/pages/Skills/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Skills: React.FC = () => {
+  return (
+    <div className="container">
+      <h1 className="text-xl font-bold">⚔️ Навыки</h1>
+      <p className="text-muted text-sm mt-1">Скоро здесь будет полная информация</p>
+    </div>
+  );
+};
+
+export default Skills;
